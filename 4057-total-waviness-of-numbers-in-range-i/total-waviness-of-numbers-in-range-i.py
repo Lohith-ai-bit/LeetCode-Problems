dcb4ac966525859d19d0ef2ce9f5525c8e1524e3
Lohith-ai-bit <kolalohithkumar@gmail.com>
2026-06-04 12:25:28 +0530

    Time: 236 ms (89.27%) | Memory: 19.5 MB (14.56%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.py
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.py
@@ -4,8 +4,6 @@ class Solution:
         for i in range(num1,num2+1):
             nums=str(i)
             for j in range(len(nums)-2):
-                if nums[j]<nums[j+1] and nums[j+1]>nums[j+2]:
-                    count+=1
-                elif nums[j]>nums[j+1] and nums[j+1]<nums[j+2]: 
+                if (nums[j]<nums[j+1] and nums[j+1]>nums[j+2]) or (nums[j]>nums[j+1] and nums[j+1]<nums[j+2]):
                     count+=1
         return count    
