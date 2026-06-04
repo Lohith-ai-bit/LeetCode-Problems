ec5a8e191ae39353482fac21c892dd1a2b852d2a
Lohith-ai-bit <kolalohithkumar@gmail.com>
2026-06-04 12:22:45 +0530

    Time: 229 ms (93.49%) | Memory: 19.4 MB (32.95%) - LeetSync

---
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.py
@@ -0,0 +1,11 @@
+class Solution:
+    def totalWaviness(self, num1: int, num2: int) -> int:
+        count=0
+        for i in range(num1,num2+1):
+            nums=str(i)
+            for j in range(len(nums)-2):
+                if nums[j]<nums[j+1] and nums[j+1]>nums[j+2]:
+                    count+=1
+                elif nums[j]>nums[j+1] and nums[j+1]<nums[j+2]: 
+                    count+=1
+        return count    
