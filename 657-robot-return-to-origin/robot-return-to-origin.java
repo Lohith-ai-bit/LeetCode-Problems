4e2427f43cf2c6a795c2d1f38f9e9f90c3cd8bd6
KOLA LOHITH KUMAR <kolalohithkumar@gmail.com>
2026-07-30 21:46:29 +0530

    Time: 11 ms (7.59%) | Memory: 45.5 MB (78.14%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int up=0;
+        int down=0;
+        int left=0;
+        int right=0;
+        for(int i=0;i<moves.length();i++){
+            if(moves.charAt(i)=='U')  up+=1;
+            if(moves.charAt(i)=='D') down+=1;
+            if(moves.charAt(i)=='L') left+=1;
+            if(moves.charAt(i)=='R') right+=1;
+        }
+        return (up==down) && (left==right);
+    }
+}
